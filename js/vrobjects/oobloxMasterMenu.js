de776640d391097cc08e32ce4e54b0dcb2fb8144
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-21 22:39:52 +0200

    Update oobloxMasterMenu.js

--- js/vrobjects/oobloxMasterMenu.js
+++ js/vrobjects/oobloxMasterMenu.js
@@ -25,7 +25,7 @@ oobloxMasterMenu = function ()
 				position.z]);
 	};
 	
-	this.mesh.fillDatGUI = function (targetScene)
+	this.mesh.fillDatGUI = function (targetScene, camera)
 	{
 		mesh.geometry.computeBoundingBox();
 		var datFolder = dat.GUIVR.create('ooblox Menu');
@@ -63,15 +63,20 @@ oobloxMasterMenu = function ()
 		
 		var rezFolder = dat.GUIVR.create('Add');
 
-
-		var posScaleRotString = "" + 	mesh.position.x + "+" + mesh.position.y + "+" + mesh.position.z + "+" +
-						mesh.scale.x + "+" + mesh.scale.y + "+" + mesh.scale.z + "+" +
-						mesh.quaternion.x + "+" + mesh.quaternion.y + "+" + mesh.quaternion.z + "+" + mesh.quaternion.w;
+		var position = new THREE.Vector3();
+		position.setFromMatrixPosition( mesh.matrixWorld );
+		var posScaleRotString = "" + 	position.x + "+" + position.y + "+" + position.z + "+1+1+1+0+0+0+1;
 		var addobj = {add: function() {
 			var d = new Date();
-			var newhref = window.location.href + "&TK" + d.getTime() + "=TTK+" + posScaleRotString + "+9+0.7+240+7+6+10";
-			window.history.pushState({}, '', newhref);
-			location.reload();}};
+			var uname = "TK" + d.getTime();
+			var newhref = window.location.href + "&" + uname + "=TTK+" + posScaleRotString + "+9+0.7+240+7+6+10";
+			window.history.replaceState({}, '', newhref);
+			var importedThing = new vrObjectConstructorList[importTypesAvailable.indexOf("TTK")]();
+			importedThing.mesh.uname = uname;
+			importedThing.load(targetScene, camera);
+
+		}};
+		
 		rezFolder.add(addobj,'add').name("Torus Knot");
 			
 		datFolder.addFolder(rezFolder);
@@ -92,7 +97,7 @@ oobloxMasterMenu = function ()
 		this.mesh.position.copy(position);
 
 		refresh(targetScene);
-		this.mesh.fillDatGUI(targetScene);
+		this.mesh.fillDatGUI(targetScene, camera);
 			
 	}
 }
