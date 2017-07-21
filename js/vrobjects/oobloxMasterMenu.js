371aeeca0f1aa132a0f14d537365738d87772bf1
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-21 15:22:11 +0200

    Update oobloxMasterMenu.js

--- js/vrobjects/oobloxMasterMenu.js
+++ js/vrobjects/oobloxMasterMenu.js
@@ -15,12 +15,24 @@ oobloxMasterMenu = function ()
 
 	var refresh = function (targetScene)
 	{
+		updateURLargs([	mesh.uname,
+				mesh.vrObjectTypeID,
+				mesh.position.x,
+				mesh.position.y,
+				mesh.position.z,
+				mesh.scale.x,
+				mesh.scale.y,
+				mesh.scale.z,
+				mesh.quaternion.x,
+				mesh.quaternion.y,
+				mesh.quaternion.z,
+				mesh.quaternion.w]);
 	};
 	
 	this.mesh.fillDatGUI = function (targetScene)
 	{
 		var datFolder = dat.GUIVR.create('ooblox Menu');
-//		datFolder.position.set(0.0, 0.0, 0.0);
+		datFolder.position.copy(mesh.position);
 		datFolder.scale.set(20.0,20.0,0.1);
 
 		var remFolder = dat.GUIVR.create('Remove');
@@ -34,10 +46,10 @@ oobloxMasterMenu = function ()
 			for (var i=0;i<urlCallParametersList.length;i++)
 			{		
 				var uname = urlCallParametersList[i].substring(0, urlCallParametersList[i].indexOf("="));
-				var obj = {	choice: uname,
+				var obj = {	myIndex: i,
 						remove: function()
 						{	
-							urlCallParametersList.splice(i, 1);
+							urlCallParametersList.splice(this.myIndex, 1);
 							var newURLstring = "?"+urlCallParametersList.join("&");
 							window.history.pushState({}, '', newURLstring);
 							location.reload();
@@ -52,18 +64,27 @@ oobloxMasterMenu = function ()
 		rezFolder.add(mesh,"rezChoice",importTypesAvailable);
 		var posFolder = dat.GUIVR.create('Rez Position');
 		var posXSlider = posFolder.add(mesh.position,'x',-200.0,200.0);
+		posXSlider.onChange(refresh);
 		var posYSlider = posFolder.add(mesh.position,'y',-200.0,200.0);
+		posYSlider.onChange(refresh);
 		var posZSlider = posFolder.add(mesh.position,'z',-200.0,200.0);
+		posZSlider.onChange(refresh);
 		rezFolder.addFolder(posFolder);
 		var scaleFolder = dat.GUIVR.create('Rez Scale');
 		var scaleXSlider = scaleFolder.add(mesh.scale,'x',0.01,20.0);
+		scaleXSlider.onChange(refresh);
 		var scaleYSlider = scaleFolder.add(mesh.scale,'y',0.01,20.0);
+		scaleYSlider.onChange(refresh);
 		var scaleZSlider = scaleFolder.add(mesh.scale,'z',0.01,20.0);
+		scaleZSlider.onChange(refresh);
 		rezFolder.addFolder(scaleFolder);
 		var rotFolder = dat.GUIVR.create('Rez Rotation');
 		var rotXSlider = rotFolder.add(mesh.rotation,'x').min(0).max(Math.PI * 2).step(0.001);
+		rotXSlider.onChange(refresh);
 		var rotYSlider = rotFolder.add(mesh.rotation,'y').min(0).max(Math.PI * 2).step(0.001);
+		rotYSlider.onChange(refresh);
 		var rotZSlider = rotFolder.add(mesh.rotation,'z').min(0).max(Math.PI * 2).step(0.001);
+		rotZSlider.onChange(refresh);
 		rezFolder.addFolder(rotFolder);
 		datFolder.addFolder(rezFolder);
 
@@ -72,6 +93,25 @@ oobloxMasterMenu = function ()
 
 	this.load = function (targetScene, camera)
 	{
+		var argList = getURLargs(this.mesh.uname);
+		var position = new THREE.Vector3();
+		position.x = parseFloat(argList[1]);
+		position.y = parseFloat(argList[2]);
+		position.z = parseFloat(argList[3]);
+		var scale = new THREE.Vector3();
+		scale.x = parseFloat(argList[4]);
+		scale.y = parseFloat(argList[5]);
+		scale.z = parseFloat(argList[6]);
+		var rotation = new THREE.Quaternion();
+		rotation.x = parseFloat(argList[7]);
+		rotation.y = parseFloat(argList[8]);
+		rotation.z = parseFloat(argList[9]);
+		rotation.w = parseFloat(argList[10]);
+
+		this.mesh.quaternion.copy(rotation);
+		this.mesh.position.copy(position);
+		this.mesh.scale.copy(scale);
+
 		refresh(targetScene);
 		this.mesh.fillDatGUI(targetScene);
 		targetScene.add(mesh);			
