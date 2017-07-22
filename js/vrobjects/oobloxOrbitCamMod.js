e160d0f96e8956c1420c56ee8b01f8e53ccc436c
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-22 09:39:40 +0200

    Update oobloxOrbitCamMod.js

--- js/vrobjects/oobloxOrbitCamMod.js
+++ js/vrobjects/oobloxOrbitCamMod.js
@@ -7,8 +7,7 @@ oobloxOrbitCamMod = function ()
 	this.mesh = new THREE.Mesh( new THREE.BoxGeometry(0, 0, 0), new THREE.MeshPhongMaterial({}));
 	this.mesh.vrObjectTypeID = "OCM";
 	var mesh = this.mesh;
-	this.cam = new THREE.Mesh( new THREE.BoxGeometry(0, 0, 0), new THREE.MeshPhongMaterial({}));
-	var cam = this.cam;
+	var cam;
 
 	var refresh = function ()
 	{
@@ -17,22 +16,21 @@ oobloxOrbitCamMod = function ()
 				cam.position.x,
 				cam.position.y,
 				cam.position.z,
-				cam.target.x,
-				cam.target.y,
-				cam.target.z]);
+				controls.target.x,
+				controls.target.y,
+				controls.target.z]);
 	}
 
 	this.load = function (targetScene, camera)
 	{
 		var argList = getURLargs(this.mesh.uname);
-		this.cam = camera;
-
-		this.cam.position.x = parseFloat(argList[1]);
-		this.cam.position.y = parseFloat(argList[2]);
-		this.cam.position.z = parseFloat(argList[3]);
-		this.cam.target.x = parseFloat(argList[4]);
-		this.cam.target.y = parseFloat(argList[5]);
-		this.cam.target.z = parseFloat(argList[6]);
+		cam = camera;
+		cam.position.x = parseFloat(argList[1]);
+		cam.position.y = parseFloat(argList[2]);
+		cam.position.z = parseFloat(argList[3]);
+		controls.target.x = parseFloat(argList[4]);
+		controls.target.y = parseFloat(argList[5]);
+		controls.target.z = parseFloat(argList[6]);
 		controls.addEventListener( 'change', refresh );
 	}
 }
