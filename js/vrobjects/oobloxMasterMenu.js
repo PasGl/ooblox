8d3f3fc2bb2250c61451bf1b852efbc60a19d694
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-22 10:45:51 +0200

    Update oobloxMasterMenu.js

--- js/vrobjects/oobloxMasterMenu.js
+++ js/vrobjects/oobloxMasterMenu.js
@@ -145,7 +145,7 @@ oobloxMasterMenu = function ()
 		datFolder.children[1].add(indicator);
 		targetScene.add( datFolder );
 		datFolder.close();
-		datFolder.children[0].addEventListener( 'pinReleased', function() {refresh(targetScene);} );
+		controls.addEventListener( 'pinReleased', function() {refresh(targetScene);} );
 	}
 
 	this.load = function (targetScene, camera)
