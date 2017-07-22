4fbb37b5a7129cb1b97dc68e1f093f11ac30e22e
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-22 09:10:47 +0200

    Update oobloxOrbitCamMod.js

--- js/vrobjects/oobloxOrbitCamMod.js
+++ js/vrobjects/oobloxOrbitCamMod.js
@@ -6,6 +6,15 @@ oobloxOrbitCamMod = function ()
 {
 	this.mesh = new THREE.Mesh( new THREE.BoxGeometry(0, 0, 0), new THREE.MeshPhongMaterial({}));
 	this.mesh.vrObjectTypeID = "OCM";
+	var cam;
+
+	var refresh = function ()
+	{
+		updateURLargs([	mesh.uname,
+				mesh.vrObjectTypeID,
+				cam.position.x,
+				cam.position.y,
+				cam.position.z]);
 
 	this.load = function (targetScene, camera)
 	{
@@ -13,7 +22,8 @@ oobloxOrbitCamMod = function ()
 		camera.position.x = parseFloat(argList[1]);
 		camera.position.y = parseFloat(argList[2]);
 		camera.position.z = parseFloat(argList[3]);
-
+		cam = camera;
+		cam.addEventListener( 'changed', function (event) {refresh();});
 	}
 }
 
