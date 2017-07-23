15f8b3de98df433dddaf1fe266b91fed403868d4
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-23 13:13:14 +0200

    Update oobloxSkybox.js

--- js/vrobjects/oobloxSkybox.js
+++ js/vrobjects/oobloxSkybox.js
@@ -54,6 +54,7 @@ oobloxSkybox = function ()
 		targetScene.add( gui );
 		console.log(gui);
 		console.log(controls);
+		console.log(dat.GUIVR);
 		gui.folder.children[2].addEventListener("onReleased", function(){ console.log("Fuck yeah 1"); });   //urlRefresh(targetScene);})
 		gui.folder.children[2].addEventListener("grabReleased", function(){ console.log("Fuck yeah 2"); });   //urlRefresh(targetScene);})
 	}
