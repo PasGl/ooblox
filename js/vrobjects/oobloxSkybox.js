30f36f8a0703c239ec8908f1e661b803c5a58be0
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-24 13:07:21 +0200

    Update oobloxSkybox.js

--- js/vrobjects/oobloxSkybox.js
+++ js/vrobjects/oobloxSkybox.js
@@ -45,22 +45,22 @@ oobloxSkybox = function ()
 
 			switch (skyboxname) {
 			    case "CloudyLightRays":
-				targetScene.fog = new THREE.Fog( 0x42474c, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0x42474c, 1, 800 );
 				break;
 			    case "DarkStormy":
-				targetScene.fog = new THREE.Fog( 0x222629, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0x222629, 1, 800 );
 				break;
 			    case "FullMoon":
-				targetScene.fog = new THREE.Fog( 0x343c3a, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0x343c3a, 1, 800 );
 				break;
 			    case "SunSet":
-				targetScene.fog = new THREE.Fog( 0x414544, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0x414544, 1, 800 );
 				break;
 			    case "ThickCloudsWater":
-				targetScene.fog = new THREE.Fog( 0x5e4b3f, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0x5e4b3f, 1, 800 );
 				break;
 			    case "TropicalSunnyDay":
-				targetScene.fog = new THREE.Fog( 0xf7f9f4, 1, 1050 );
+				targetScene.fog = new THREE.Fog( 0xf7f9f4, 1, 800 );
 				break;
 			}
 
