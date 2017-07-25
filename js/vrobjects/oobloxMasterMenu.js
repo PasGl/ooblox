c8c319c799bb4d30b6680bf4a743592cc1bb872c
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-26 00:12:09 +0200

    Update oobloxMasterMenu.js

--- js/vrobjects/oobloxMasterMenu.js
+++ js/vrobjects/oobloxMasterMenu.js
@@ -75,7 +75,7 @@ oobloxMasterMenu = function ()
 			var posScaleRotString = "" + position.x  + "+" + position.y + "+" + position.z;
 			var d = new Date();
 			var uname = "TK" + d.getTime();
-			var newhref = window.location.href + "&" + uname + "=TTK+" + posScaleRotString + "+9+0.7+240+7+"+Math.floor(Math.random() * 25)+"+"+Math.floor(Math.random() * 25);
+			var newhref = window.location.href + "&" + uname + "=TTK+" + posScaleRotString + "+6+0.4+240+7+"+Math.floor(Math.random() * 25)+"+"+Math.floor(Math.random() * 25);
 			window.history.pushState({}, '', newhref);
 			var importedThing = new vrObjectConstructorList[importTypesAvailable.indexOf("TTK")]();
 			importedThing.mesh.uname = uname;
