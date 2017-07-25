8d0654dbbd112805e7e6fe433da843ac3b8d5798
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-25 10:04:25 +0200

    Update oobloxPSOLSystem.js

--- js/vrobjects/oobloxPSOLSystem.js
+++ js/vrobjects/oobloxPSOLSystem.js
@@ -325,16 +325,10 @@ function PSOLSystem ()
 					stepvec.applyQuaternion(currentTurtle.orientation);
 
 					tubePoints.push(currentTurtle.position.clone().add(stepvec));
-					tubeRadii.push(currentTurtle.scale.clone());
+					tubeRadii.push(currentTurtle.scale.clone().addScalar(0.6180339887));
 
-				//	tubePoints.push(currentTurtle.position.clone().add(stepvec).add(stepvec));
-				//	tubeRadii.push(currentTurtle.scale.clone());
-
-
-					//stepvec = new THREE.Vector3(0,currentTurtle.scale.x*5.0,0);
-					//stepvec.applyQuaternion(currentTurtle.orientation);
-					//tubePoints.push(currentTurtle.position.clone().add(stepvec));
-					//tubeRadii.push(new THREE.Vector3(0,0,0));
+					tubePoints.push(currentTurtle.position.clone().add(stepvec*1.6180339887));
+					tubeRadii.push(new THREE.Vector3(0,0,0));
 
 
 					if (tubePoints.length>1)
