a24f95537e8129fc0207cd45104c7e21917b2128
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-25 09:18:49 +0200

    Update oobloxPSOLSystem.js

--- js/vrobjects/oobloxPSOLSystem.js
+++ js/vrobjects/oobloxPSOLSystem.js
@@ -124,7 +124,7 @@ function PSOLSystem ()
 		closingGeometryMatrix.compose(
 			turtle.position.clone().add( (new THREE.Vector3( 0, step*0.5, 0 )).applyQuaternion(turtle.orientation)),
 			turtle.orientation.clone(),
-			(new THREE.Vector3( turtle.scale.x, turtle.scale.x, turtle.scale.x )));
+			(new THREE.Vector3( turtle.scale.x*2.0, turtle.scale.x*2.0, turtle.scale.x*2.0 )));
 		closingGeometry.applyMatrix(closingGeometryMatrix);
 		return closingGeometry;
 	}
