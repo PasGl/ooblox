0484ef699020d227cf452b95e236c8fcccec2d26
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-27 13:47:24 +0200

    Update ooblox.js

--- js/ooblox.js
+++ js/ooblox.js
@@ -362,7 +362,6 @@ function showHideMenus()
 	scene.traverse(function(obj) {
 		if (obj.hasOwnProperty('beingMoved'))
 		{
-			console.log(obj);
 			if (obj.parent.name==="vrObjectGroup")
 			{
 				obj.visible = !menusHidden;
