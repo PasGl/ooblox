985fb3bea48eae0b0277f508dcd34b116488947e
Pascal Gleske <PasGl@users.noreply.github.com>
2017-07-30 13:19:36 +0200

    Add files via upload

---
+++ js/vrobjects/oobloxTexturePanel.js
@@ -0,0 +1,109 @@
+/**
+ * @author Pascal Gleske / https://github.com/PasGl
+ */
+
+oobloxTexturePanel = function ()
+{
+	this.mesh = new THREE.Mesh( new THREE.PlaneGeometry(1, 1, 10, 10), new THREE.MeshPhongMaterial({}));
+	this.mesh.vrObjectTypeID = "TPL";
+	this.mesh.uname = "";
+	var mesh = this.mesh;
+
+	var groupNode = new THREE.Group();
+	groupNode.add(this.mesh);
+	groupNode.name = "vrObjectGroup";
+	var guioffset = new THREE.Vector3();
+
+	var datFolder = dat.GUIVR.create('Texture Panel');
+	groupNode.add( datFolder )
+
+	var TPLProperties = function ()
+	{
+		this.followGUI = true;	
+	}
+
+	var conf = new TPLProperties();
+
+
+	var refreshURL = function (targetScene)
+	{
+		var position = new THREE.Vector3();
+		targetScene.updateMatrixWorld();
+		position.setFromMatrixPosition( mesh.matrixWorld );
+		var guiposition = new THREE.Vector3();
+		guiposition.setFromMatrixPosition( datFolder.matrixWorld );
+
+		if (conf.followGUI)
+		{
+			mesh.position.copy(guiposition.sub(guioffset));
+		}
+		else
+		{
+			guioffset.copy(guiposition.sub(position));
+		}
+
+		updateURLargs([	mesh.uname,
+				mesh.vrObjectTypeID,
+				mesh.position.x,
+				mesh.position.y,
+				mesh.position.z,
+				mesh.scale.x,
+				mesh.scale.y,
+				mesh.rotation.x,
+				mesh.rotation.y,
+				mesh.rotation.z,
+				guioffset.x,
+				guioffset.y,
+				guioffset.z,
+				encodeURIComponent(conf.textureFilename)]);
+	}
+
+	var refresh = function (targetScene)
+	{
+		mesh.material.map = new THREE.TextureLoader().load( conf.textureFilename );
+		refreshURL(targetScene);
+	}
+
+	this.mesh.fillDatGUI = function (targetScene,mesh)
+	{
+		datFolder.position.copy(guioffset).add(mesh.position);
+		datFolder.scale.set(20.0,20.0,0.1);
+		var followFlag = datFolder.add(conf,'followGUI');
+
+		var propFolder = dat.GUIVR.create('Properties');
+		var scxSlider = propFolder.add(mesh.scale,'x',0.0001,100);
+		scxSlider.onChange(function(){refreshURL(targetScene);});
+		var scySlider = propFolder.add(mesh.scale,'y',0.0001,100);
+		scySlider.onChange(function(){refreshURL(targetScene);});
+		var rotxSlider = propFolder.add(mesh.scale,'x',0.0001,100);
+		rotxSlider.onChange(function(){refreshURL(targetScene);});
+		var rotySlider = propFolder.add(mesh.scale,'y',0.0001,100);
+		rotySlider.onChange(function(){refreshURL(targetScene);});
+		var rotzSlider = propFolder.add(mesh.scale,'z',0.0001,100);
+		rotzSlider.onChange(function(){refreshURL(targetScene);});
+
+		targetScene.add( groupNode );
+		window.addEventListener("mouseup", function(){refreshURL(targetScene);})
+	}
+
+	this.load = function (targetScene, camera)
+	{
+		var argList = getURLargs(this.mesh.uname);
+		this.mesh.position.x = parseFloat(argList[1]);
+		this.mesh.position.y = parseFloat(argList[2]);
+		this.mesh.position.z = parseFloat(argList[3]);
+		this.mesh.scale.x = parseFloat(argList[4]);
+		this.mesh.scale.y = parseFloat(argList[5]);
+		this.mesh.rotation.x = parseFloat(argList[6]);
+		this.mesh.rotation.y = parseFloat(argList[7]);
+		this.mesh.rotation.z = parseFloat(argList[8]);
+		guioffset.x = parseFloat(argList[9]);
+		guioffset.y = parseFloat(argList[10]);
+		guioffset.z = parseFloat(argList[11]);
+		conf.textureFilename = "./"+decodeURIComponent(argList.slice(12).join(""));
+		this.mesh.fillDatGUI(targetScene,this.mesh);
+		refresh(targetScene);
+	}
+}
+
+vrObjectConstructorList.push(oobloxTexturePanel); // global list of all available vrObject type constructors
