081296afcdf39a7f1e422b1a263fe08ce051b2d3
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-02 12:59:31 +0200

    Update oobloxMasterMenu.js

--- js/vrobjects/oobloxMasterMenu.js
+++ js/vrobjects/oobloxMasterMenu.js
@@ -166,6 +166,21 @@ oobloxMasterMenu = function ()
 			refresh(targetScene);}};
 		rezFolder.add(texobj,'add').name("Texture Panel");
 
+		var grnobj = {add: function() {
+			var position = new THREE.Vector3();
+			targetScene.updateMatrixWorld();
+			position.setFromMatrixPosition( indicator.matrixWorld );
+			var posScaleRotString = "" + position.x  + "+" + position.y + "+" + position.z;
+			var d = new Date();
+			var uname = "DSG" + d.getTime();
+			var newhref = window.location.href + "&" + uname + "=DSG+" + posScaleRotString + "+200.0+200.0+5.0+0+0+-10+2";
+			window.history.pushState({}, '', newhref);
+			var importedThing = new vrObjectConstructorList[importTypesAvailable.indexOf("DSG")]();
+			importedThing.mesh.uname = uname;
+			importedThing.load(targetScene, camera);
+			refresh(targetScene);}};
+		rezFolder.add(grnobj,'add').name("Ground");
+
 		datFolder.addFolder(rezFolder);
 		datFolder.children[1].add(mesh);
 		datFolder.children[1].add(indicator);
