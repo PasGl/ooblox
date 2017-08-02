0f2677f9d10f6ad7b575855c5abd8ca1f39c4edb
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-02 15:11:26 +0200

    Update oobloxDiamondSquareGround.js

--- js/vrobjects/oobloxDiamondSquareGround.js
+++ js/vrobjects/oobloxDiamondSquareGround.js
@@ -8,7 +8,7 @@
 
 oobloxDiamondSquareGround = function ()
 {
-	this.mesh = new THREE.Mesh( new THREE.PlaneGeometry(1, 1, 10, 10), new THREE.MeshStandardMaterial({}));
+	this.mesh = new THREE.Mesh( new THREE.PlaneGeometry(1, 1, 1, 1), new THREE.MeshStandardMaterial({}));
 	this.mesh.rotation.x = -Math.PI * 0.5;
 	this.mesh.vrObjectTypeID = "DSG";
 	this.mesh.uname = "";
@@ -20,9 +20,66 @@ oobloxDiamondSquareGround = function ()
 	var guioffset = new THREE.Vector3();
 
 	var datFolder = dat.GUIVR.create('Ground (Texures by Nobiax)');
-	groupNode.add( datFolder )
+	groupNode.add( datFolder );
 
-	var TPLProperties = function ()	{this.followGUI = true;this.theme = "1";}
+	var heightMaps = [[[0.0,0.0],[0.0,0.0]]];
+	var geometries = [new THREE.PlaneGeometry(1, 1, 1, 1)];
+
+	var addIteration = function ()
+	{
+		var iterationsSoFar = heightMaps.length;
+		lastheightMap = heightMaps[iterationsSoFar-1];
+		var nextHeightMap = [];
+
+
+		for(var i = 0; i < lastheightMap.length -1; i ++)
+		{
+			var thisRow = [];
+			var newRow = [];
+			for(var j = 0; j < lastheightMap[i].length -1; j ++)
+			{
+				thisRow.push(lastheightMap[i][j]);
+				thisRow.push(0.0);
+				newRow.push(0.0);
+				newRow.push(0.0);
+			}
+			thisRow.push(lastheightMap[i][lastheightMap[i].length -1]);
+			newRow.push(0.0);
+			nextHeightMap.push(thisRow);
+			nextHeightMap.push(newRow);
+		}
+		
+		var lastRow = [];
+		for(var j = 0; j < lastheightMap[lastheightMap.length -1].length -1; j ++)
+		{
+			lastRow.push(lastheightMap[lastheightMap.length -1][j]);
+			lastRow.push(0.0);
+		}
+		lastRow.push(lastheightMap[lastheightMap.length -1][lastheightMap[lastheightMap.length -1].length -1]);
+		nextHeightMap.push(lastRow);
+
+		heightMaps.push(nextHeightMap);
+		geometries.push(new THREE.PlaneGeometry(1, 1, Math.pow(2,iterationsSoFar+1), Math.pow(2,iterationsSoFar+1));
+
+		var geoindex = 0;
+		for(var i = 0; i < nextHeightMap.length; i ++)
+		{
+			for(var j = 0; j < nextHeightMap[i].length; j ++)
+			{
+				geometries[geometries.length-1].vertices[geoindex].position.z = nextHeightMap[i][j];
+				geoindex++;
+			}
+		}
+	}
+
+	var TPLProperties = function ()	{
+		this.followGUI = true;
+		this.theme = "1";
+		this.randomSeed = 1234567;
+		this.iterations = 0;
+		this.textureRepsX = 2.0;
+		this.textureRepsY = 2.0;
+	};
 	var conf = new TPLProperties();
 
 	var themes = ["1","2","3","4","5"];
@@ -76,15 +133,15 @@ oobloxDiamondSquareGround = function ()
 		mesh.material.map = new THREE.TGALoader().load( "images/3D_pattern_53/" + textureFolder + "/diffuse.tga");
 		mesh.material.map.wrapS = THREE.RepeatWrapping;
 		mesh.material.map.wrapT = THREE.RepeatWrapping;
-		mesh.material.map.repeat.set( 10.0 , 10.0);
+		mesh.material.map.repeat.set( conf.textureRepsX , conf.textureRepsY);
 		mesh.material.normalMap = new THREE.TGALoader().load( "images/3D_pattern_53/" + textureFolder + "/normal.tga");
 		mesh.material.normalMap.wrapS = THREE.RepeatWrapping;
 		mesh.material.normalMap.wrapT = THREE.RepeatWrapping;
-		mesh.material.normalMap.repeat.set( 10.0 , 10.0);
+		mesh.material.normalMap.repeat.set( conf.textureRepsX , conf.textureRepsY);
 		mesh.material.emissiveMap = new THREE.TGALoader().load( "images/3D_pattern_53/" + textureFolder + "/specular.tga");
 		mesh.material.emissiveMap.wrapS = THREE.RepeatWrapping;
 		mesh.material.emissiveMap.wrapT = THREE.RepeatWrapping;
-		mesh.material.emissiveMap.repeat.set( 10.0 , 10.0);
+		mesh.material.emissiveMap.repeat.set( conf.textureRepsX , conf.textureRepsY);
 		mesh.material.emissive = new THREE.Color( 0x555555 );
 		mesh.material.metalness = 0.1;
 		mesh.material.roughness = 0.5;
@@ -133,6 +190,10 @@ oobloxDiamondSquareGround = function ()
 		refresh(targetScene);
 		var event = new Event('vrObjectInstantiated');
 		document.dispatchEvent(event);
+
+		console.log(heightMaps);
+		addIteration();
+		console.log(heightMaps);
 	}
 }
 
