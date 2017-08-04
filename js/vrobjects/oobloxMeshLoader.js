f171c538b2918fcb5f3d4ce5607ffb1af4027c96
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-04 12:59:02 +0200

    Update oobloxMeshLoader.js

--- js/vrobjects/oobloxMeshLoader.js
+++ js/vrobjects/oobloxMeshLoader.js
@@ -8,18 +8,17 @@ oobloxMeshLoader = function ()
 	this.mesh.vrObjectTypeID = "OML";
 	this.mesh.uname = "";
 	var mesh = this.mesh;
+	
+	var loadedModel = new THREE.Mesh( new THREE.PlaneGeometry(1, 1, 10, 10), new THREE.MeshStandardMaterial({}));
+	mesh.add(loadedModel);
 
-	var TPLProperties = function ()	{this.followGUI = true;this.modelFilename = "Object.dae";}
+	var TPLProperties = function ()	{this.followGUI = true;this.modelFilename = "Object.dae";this.models=["Object.dae"];}
 	var conf = new TPLProperties();
 
 	var groupNode = new THREE.Group();
 	groupNode.add(this.mesh);
 	groupNode.name = "vrObjectGroup";
 	var guioffset = new THREE.Vector3();
-
-	var datFolder,followFlag,remobj,propFolder,sourceChanger,scxSlider,scySlider,sczSlider,rotxSlider,rotySlider,rotzSlider;
-
-	var models = [];
 
 	var refreshURL = function (targetScene,mesh)
 	{
@@ -51,56 +50,18 @@ oobloxMeshLoader = function ()
 
 	var refresh = function (targetScene,mesh)
 	{
-		var uname = mesh.uname;
-		var vrObjectTypeID = mesh.vrObjectTypeID;
-		var position = (new THREE.Vector3()).copy(mesh.position);
-		var scale = (new THREE.Vector3()).copy(mesh.scale);
-		var rotation = (new THREE.Quaternion()).copy(mesh.rotation);
-
+		mesh.remove(loadedModel);
 		if ([".dae",".DAE"].indexOf(conf.modelFilename.substring(conf.modelFilename.length-4,conf.modelFilename.length+1)) >=0)
 		{
 			var loader = new THREE.ColladaLoader();
 			loader.load('models/'+conf.modelFilename, function ( collada ) 
 			{
-				groupNode.remove(mesh);
-				mesh = collada.scene;
-				mesh.position.copy(position);
-				mesh.scale.copy(scale);
-				mesh.rotation.copy(rotation);
-				mesh.uname = uname;
-				mesh.vrObjectTypeID = vrObjectTypeID;
-				groupNode.add(mesh);
-				fillDatGUI(targetScene,mesh);
+				loadedModel = collada.scene;
+				
 			});
-		}	
-	}
-
-	var fillDatGUI = function (targetScene,mesh)
-	{
-		propFolder.remove(sourceChanger);
-		propFolder.remove(scxSlider);
-		propFolder.remove(scySlider);
-		propFolder.remove(sczSlider);
-		propFolder.remove(rotxSlider);
-		propFolder.remove(rotySlider);
-		propFolder.remove(rotzSlider);
-		datFolder.remove(propFolder);
-		propFolder = dat.GUIVR.create('Properties');
-		sourceChanger = propFolder.add(conf,'modelFilename',models);
-		sourceChanger.onChange(function(value) {refresh(targetScene, mesh);});
-		scxSlider = propFolder.add(mesh.scale,'x',0.0001,100).name("Scale X");
-		scxSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		scySlider = propFolder.add(mesh.scale,'y',0.0001,100).name("Scale Y");
-		scySlider.onChange(function(){refreshURL(targetScene, mesh);});
-		sczSlider = propFolder.add(mesh.scale,'z',0.0001,100).name("Scale Z");
-		sczSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotxSlider = propFolder.add(mesh.rotation,'x',0.0,Math.PI*2.0).name("Rotation X").step(0.0001);
-		rotxSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotySlider = propFolder.add(mesh.rotation,'y',0.0,Math.PI*2.0).name("Rotation Y").step(0.0001);
-		rotySlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotzSlider = propFolder.add(mesh.rotation,'z',0.0,Math.PI*2.0).name("Rotation Z").step(0.0001);
-		rotzSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		datFolder.addFolder(propFolder);
+		}
+		mesh.add(loadedModel);
+		refreshURL(targetScene,mesh);
 	}
 
 	this.load = function (targetScene, camera)
@@ -119,47 +80,23 @@ oobloxMeshLoader = function ()
 		guioffset.y = parseFloat(argList[11]);
 		guioffset.z = parseFloat(argList[12]);
 		conf.modelFilename = decodeURIComponent(argList.slice(13).join(""));
-
-		datFolder = dat.GUIVR.create('Mesh');
-		datFolder.position.copy(guioffset).add(mesh.position);
-		datFolder.scale.set(20.0,20.0,0.1);
-		followFlag = datFolder.add(conf,'followGUI');
-		remobj = {myuname: mesh.uname,remove: function(){removeInstance(this.myuname);}};
-		datFolder.add(remobj,'remove').name(mesh.uname);
-		propFolder = dat.GUIVR.create('Properties');
-		sourceChanger = propFolder.add(conf,'modelFilename',models);
-		sourceChanger.onChange(function(value) {refresh(targetScene, mesh);});
-		scxSlider = propFolder.add(mesh.scale,'x',0.0001,100).name("Scale X");
-		scxSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		scySlider = propFolder.add(mesh.scale,'y',0.0001,100).name("Scale Y");
-		scySlider.onChange(function(){refreshURL(targetScene, mesh);});
-		sczSlider = propFolder.add(mesh.scale,'z',0.0001,100).name("Scale Z");
-		sczSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotxSlider = propFolder.add(mesh.rotation,'x',0.0,Math.PI*2.0).name("Rotation X").step(0.0001);
-		rotxSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotySlider = propFolder.add(mesh.rotation,'y',0.0,Math.PI*2.0).name("Rotation Y").step(0.0001);
-		rotySlider.onChange(function(){refreshURL(targetScene, mesh);});
-		rotzSlider = propFolder.add(mesh.rotation,'z',0.0,Math.PI*2.0).name("Rotation Z").step(0.0001);
-		rotzSlider.onChange(function(){refreshURL(targetScene, mesh);});
-		datFolder.addFolder(propFolder);
-		groupNode.add( datFolder );
 		targetScene.add( groupNode );
-
+				
 		$.get("./models", function(data) {
-			models = data.split("href=\"");
+			conf.models = data.split("href=\"");
 			var n = 0;
-			while (n<models.length)
+			while (n<conf.models.length)
 			{
-				var thisfilename = models[n].substring(0,models[n].indexOf("\""));
+				var thisfilename = conf.models[n].substring(0,conf.models[n].indexOf("\""));
 
 				if ( [".dae",".DAE",".obj",".OBJ",".stl",".STL"].indexOf(thisfilename.substring(thisfilename.length-4,thisfilename.length+1)) >=0)
 				{
-					models[n] = thisfilename;
+					conf.models[n] = thisfilename;
 					n++;
 				}
-				else models.splice(n,1);
+				else conf.models.splice(n,1);
 			}
-			refresh(targetScene);
+			refresh(targetScene,mesh);
 			window.addEventListener("mouseup", function(){refreshURL(targetScene);});
 			var event = new Event('vrObjectInstantiated');
 			document.dispatchEvent(event);
