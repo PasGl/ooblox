f74df5b67a68948a0617a2386893483367a917e7
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-05 11:18:16 +0200

    Update ooblox.js

--- js/ooblox.js
+++ js/ooblox.js
@@ -250,9 +250,6 @@ function createScene()
 	scene = new THREE.Scene();
 	scene.name = "Scene";
 	scene.fog = new THREE.Fog( 0xbfd1e5, 1, 1050 );
-	//var hemiLight = new THREE.HemisphereLight( 0xbfd1e5, 0x9d8851, 0.6 );
-	//hemiLight.position.set( -400, 1400, 400 );
-	//scene.add(hemiLight);
 	dirTarget = new THREE.Object3D();
 	dirTarget.name = "dirTarget";
 	scene.add( dirTarget );
