6f85219c75816f4556eb34dbb3c5283730c23792
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-10 03:35:38 +0200

    Update oobloxTexturePanel.js

--- js/vrobjects/oobloxTexturePanel.js
+++ js/vrobjects/oobloxTexturePanel.js
@@ -77,10 +77,10 @@ oobloxTexturePanel = function ()
 		var sourceChanger = propFolder.add(conf,'textureFilename',textures);
 		sourceChanger.onChange(function() {refresh(targetScene);});
 
-		//mesh.material.transparent = conf.transparent
-		//mesh.material.opacity = conf.opacity;
-		//mesh.material.lights = conf.lights;
-		//mesh.material.needsUpdate=true;
+		mesh.material.transparent = conf.transparent
+		mesh.material.opacity = conf.opacity;
+		mesh.material.lights = conf.lights;
+		mesh.material.needsUpdate=true;
 
 		var transSwitch = propFolder.add(conf,'transparent').name("Transparent");
 		transSwitch.onChange(function(){refreshURL(targetScene);});
