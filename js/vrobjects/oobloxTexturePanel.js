d121051d2b72a70ab1391de0a28da729d87b0234
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-10 03:20:37 +0200

    Update oobloxTexturePanel.js

--- js/vrobjects/oobloxTexturePanel.js
+++ js/vrobjects/oobloxTexturePanel.js
@@ -115,9 +115,9 @@ oobloxTexturePanel = function ()
 		guioffset.x = parseFloat(argList[9]);
 		guioffset.y = parseFloat(argList[10]);
 		guioffset.z = parseFloat(argList[11]);
-		mesh.material.transparent = Boolean(argList[12]=="true");
-		mesh.material.opacity = parseFloat(argList[13]);
-		mesh.material.lights = Boolean(argList[14]=="true");
+		//mesh.material.transparent = Boolean(argList[12]=="true");
+		//mesh.material.opacity = parseFloat(argList[13]);
+		//mesh.material.lights = Boolean(argList[14]=="true");
 		conf.textureFilename = decodeURIComponent(argList.slice(15).join(""));
 		$.get("./images/textures", function(data) {
 			textures = data.split("href=\"");
