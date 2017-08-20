9210c0cb7cf77dab5eb1531d6f0219b4ad93baad
Pascal Gleske <PasGl@users.noreply.github.com>
2017-08-20 13:43:16 +0200

    Update oobloxPSOLSystem.js

--- js/vrobjects/oobloxPSOLSystem.js
+++ js/vrobjects/oobloxPSOLSystem.js
@@ -157,9 +157,10 @@ function PSOLSystem ()
 		var tsx = turtle.scale.x * 30.0;
 		var closingGeometry = new THREE.BoxGeometry(tsx*2.0,tsx*2.0,0.0,1,1,1);
 
-		closingGeometry.faceVertexUvs[2] = [new THREE.UV(1, 1),new THREE.UV(1, 0),new THREE.UV(0, 1)];
-		closingGeometry.faceVertexUvs[3] = [new THREE.UV(1, 0),new THREE.UV(0, 0),new THREE.UV(0, 1)];
-		closingGeometry.uvsNeedUpdate = true;
+console.log(closingGeometry);
+//		closingGeometry.faceVertexUvs[2] = [new THREE.UV(1, 1),new THREE.UV(1, 0),new THREE.UV(0, 1)];
+//		closingGeometry.faceVertexUvs[3] = [new THREE.UV(1, 0),new THREE.UV(0, 0),new THREE.UV(0, 1)];
+//		closingGeometry.uvsNeedUpdate = true;
 
 		var closingGeometryMatrix = new THREE.Matrix4 ();
 		closingGeometryMatrix.compose(
